12bb68a9a933343d8e1d91779a4d3087dc528d66
Kadet <kadet1090@gmail.com>
2014-04-21 14:25:18 +0200

    Added isset and unset abilities to Property.

--- Property.php
+++ Property.php
@@ -43,6 +43,42 @@ trait Property {
         }
     }
 
+    public function __unset($name) {
+        $reflection = new \ReflectionClass(get_called_class());
+        try {
+            $unsetter = '_unset_'.$name;
+            $method = $reflection->getMethod($unsetter);
+
+            if($this->hasAccess($method, $this->getCaller()))
+                return $this->$unsetter();
+            else
+                throw new \RuntimeException('Cannot unset ' . ($method->isPrivate() ? 'private' : 'protected') . ' property ' . get_class($this) . '::$' . $name);
+        } catch (\ReflectionException $exception) {
+            if (method_exists($this, '_unset'))
+                return $this->_unset($name);
+            else
+                throw new \RuntimeException('Trying to unset non-existent property ' . get_class($this) . '::$' . $name);
+        }
+    }
+
+    public function __isset($name) {
+        $reflection = new \ReflectionClass(get_called_class());
+        try {
+            $issetter = '_isset_'.$name;
+            $method = $reflection->getMethod($issetter);
+
+            if($this->hasAccess($method, $this->getCaller()))
+                return $this->$issetter();
+            else
+                return false;
+        } catch (\ReflectionException $exception) {
+            if (method_exists($this, '_isset'))
+                return $this->_isset($name);
+            else
+                return false;
+        }
+    }
+
     private function hasAccess(\ReflectionMethod $method, $caller) {
         return $method->isPublic() ||
         ($method->isProtected() && $caller == get_called_class()) ||
